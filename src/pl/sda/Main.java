55989fd75ffd6c834a61d8b6f8fa3421e9f958d6
MarczakMateusz <Mateusz.Szymon.Marczak@gmail.com>
2018-03-17 23:00:19 +0100

    debug commit2

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -12,7 +12,8 @@ public class Main {
     public static void main(String[] args) {
         App app = new App();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Prosze podac nazwe kategorii w serwisie \"Ceneo\" zaczynajaca sie wielka litera");
+        System.out.println("Prosze podac url wybranej kategorii w serwisie \"Ceneo\" zaczynajaca sie wielka litera");
+        System.out.println("Na przykład \"Gitary_i_akcesoria/p:Yamaha_Music;0191\"");
         String url = sc.nextLine();
         url = url.replaceAll(" ","_");
         url = "https://www.ceneo.pl/"+url;
